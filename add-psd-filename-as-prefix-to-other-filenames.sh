664eef3672b57a63b28e7a216640037ded73dfc3
Tal Jacob <taljacob2@gmail.com>
2023-07-04 09:32:53 +0300

    Edit Doc Format
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- add-psd-filename-as-prefix-to-other-filenames.sh
+++ add-psd-filename-as-prefix-to-other-filenames.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
 
 : '
-    <#
-    .DESCRIPTION
+    Description:
     A script that scans for a given file. Retrieves its name (without extension),
     and adds it as a prefix, followed by an underscore, to all the file names in
     its directory, that have a certain given file extension.
 
-    .PARAMETER PathOfFileToTakeItsNameAsPrefix
+    Parameters:
+    $1: pathOfFileToTakeItsNameAsPrefix
     Specify the path to the target file to retrieve its name, that will serve as
     a prefix to all the files with the given
-    `FileExtensionToAddThePrefixToTheirName` extension in its directory.
+    `fileExtensionToAddThePrefixToTheirName` extension in its directory.
 
-    .PARAMETER FileExtensionToAddThePrefixToTheirName
+    $2: fileExtensionToAddThePrefixToTheirName
     Specify the target file extension, so all of the files with this file
     extension that are located in the directory of the given
-    `PathOfFileToTakeItsNameAsPrefix` will be renamed to have a prefix, that
-    will include the file `PathOfFileToTakeItsNameAsPrefix`, followed by an
+    `pathOfFileToTakeItsNameAsPrefix` will be renamed to have a prefix, that
+    will include the file `pathOfFileToTakeItsNameAsPrefix`, followed by an
     underscore.
 
-    .NOTES
+    Notes:
     MIT License
     Author: Tal Jacob
 
-    .EXAMPLE
+    Example Of Use:
+    ```
     addFilenameAsPrefixToOtherFilenames "/mnt/demo.txt" "jpg"
-    #>
+    ```
 '
 addFilenameAsPrefixToOtherFilenames() {
     pathOfFileToTakeItsNameAsPrefix="$1"
