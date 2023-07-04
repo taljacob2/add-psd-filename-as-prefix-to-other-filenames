2aa64a18493c17d968e737e3398ec0e374b80275
Tal Jacob <taljacob2@gmail.com>
2023-07-04 08:00:48 +0300

    Add Doc And Make As Function
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- add-filename-as-prefix-to-other-filenames.ps1
+++ add-filename-as-prefix-to-other-filenames.ps1
@@ -1,13 +1,50 @@
-param (
-    [parameter(mandatory)][Alias("File")][string]$PathOfFileToTakeItsNameAsPrefix,
-    [parameter(mandatory)][Alias("Ext", "Extension")][string]$FileExtensionToAddThePrefixToTheirName
-)
+function Add-Filename-As-Prefix-To-Other-Filenames {
+    <#
+    .DESCRIPTION
+    A script that scans for a given file. Retrieves its name (without extension),
+    and adds it as a prefix, followed by an underscore, to all the file names in
+    its directory, that have a certain given file extension.
 
-$file = Get-Item $PathOfFileToTakeItsNameAsPrefix
+    .PARAMETER PathOfFileToTakeItsNameAsPrefix
+    Specify the path to the target file to retrieve its name, that will serve as
+    a prefix to all the files with the given
+    `FileExtensionToAddThePrefixToTheirName` extension in its directory.
 
-Get-ChildItem $file.Directory | ForEach { 
-    $fileNameWithoutExtention = [System.IO.Path]::GetFileNameWithoutExtension($file)
-    if ($_.Name.EndsWith(".$FileExtensionToAddThePrefixToTheirName")){
-        $_ | Rename-Item -NewName "$fileNameWithoutExtention`_$_"
+    .PARAMETER FileExtensionToAddThePrefixToTheirName
+    Specify the target file extension, so all of the files with this file
+    extension that are located in the directory of the given
+    `PathOfFileToTakeItsNameAsPrefix` will be renamed to have a prefix, that
+    will include the file `PathOfFileToTakeItsNameAsPrefix`, followed by an
+    underscore.
+
+    .INPUTS
+    None. You cannot pipe objects to this script.
+
+    .OUTPUTS
+    None. This script does not generate any output.
+
+    .NOTES
+    MIT License
+    Author: Tal Jacob
+
+    .EXAMPLE
+    Add-Filename-As-Prefix-To-Other-Filenames -Path "C:\Program Files\demo.txt" -Ext "jpg"
+
+    .LINK
+    Online version: https://github.com/taljacob2/add-filename-as-prefix-to-other-filenames
+    #>
+
+    param (
+        [parameter(mandatory)][Alias("Path")][string]$PathOfFileToTakeItsNameAsPrefix,
+        [parameter(mandatory)][Alias("Ext", "Extension")][string]$FileExtensionToAddThePrefixToTheirName
+    )
+
+    $file = Get-Item $PathOfFileToTakeItsNameAsPrefix
+
+    Get-ChildItem $file.Directory | ForEach { 
+        $fileNameWithoutExtention = [System.IO.Path]::GetFileNameWithoutExtension($file)
+        if ($_.Name.EndsWith(".$FileExtensionToAddThePrefixToTheirName")){
+            $_ | Rename-Item -NewName "$fileNameWithoutExtention`_$_"
+        }
     }
 }
