f0604477fbfa22ec9ccfe2d121fc85d6fc2a684c
Tal Jacob <taljacob2@gmail.com>
2023-07-04 09:29:39 +0300

    Remove Redundant Doc
    Signed-off-by: Tal Jacob <taljacob2@gmail.com>

--- add-psd-filename-as-prefix-to-other-filenames.sh
+++ add-psd-filename-as-prefix-to-other-filenames.sh
@@ -12,9 +12,6 @@
     a prefix to all the files with the given
     `FileExtensionToAddThePrefixToTheirName` extension in its directory.
 
-    .PARAMETER Item
-    Another way to send the `PathOfFileToTakeItsNameAsPrefix` as a parameter.
-
     .PARAMETER FileExtensionToAddThePrefixToTheirName
     Specify the target file extension, so all of the files with this file
     extension that are located in the directory of the given
@@ -22,18 +19,12 @@
     will include the file `PathOfFileToTakeItsNameAsPrefix`, followed by an
     underscore.
 
-    .INPUTS
-    None. You cannot pipe objects to this script.
-
-    .OUTPUTS
-    None. This script does not generate any output.
-
     .NOTES
     MIT License
     Author: Tal Jacob
 
     .EXAMPLE
-    Add-Filename-As-Prefix-To-Other-Filenames -Path "C:\Program Files\demo.txt" -Ext "jpg"
+    addFilenameAsPrefixToOtherFilenames "/mnt/demo.txt" "jpg"
     #>
 '
 addFilenameAsPrefixToOtherFilenames() {
@@ -41,17 +32,9 @@ addFilenameAsPrefixToOtherFilenames() {
     fileExtensionToAddThePrefixToTheirName="$2"
 
     FILE_PATH="$pathOfFileToTakeItsNameAsPrefix"
-    
-    : '
-        More info about string extraction:
-        - [here](https://stackoverflow.com/a/965069/14427765).
-        - [here](https://stackoverflow.com/a/44350542/14427765).
-    '
     FILE_PATH_WITHOUT_EXTENSION=$(echo "${FILE_PATH%.*}")
     FILE_LAST_DIR_PATH=$(echo "${FILE_PATH%/*}")
     FILE_NAME_WITHOUT_EXTENSION=$(echo "${FILE_PATH_WITHOUT_EXTENSION#$FILE_LAST_DIR_PATH/}")
-    FILE_NAME=$(echo "${FILE_PATH#$FILE_LAST_DIR_PATH/}")
-    FILE_EXTENSION=$(echo "${FILE_PATH#*.}")
 
     for fileToRename in *."$fileExtensionToAddThePrefixToTheirName"; do
         if [ -f "$fileToRename" ]; then
